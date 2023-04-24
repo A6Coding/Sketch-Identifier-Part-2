7bd726c0b143f8f359d7ff093c4b61bbea018f10
A6Coding <111938126+A6Coding@users.noreply.github.com>
2023-04-23 20:31:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white"); //makes canvas white
+}
+
+function clear() {
+    background("white");
+}
+
+function draw() {
+
+}
